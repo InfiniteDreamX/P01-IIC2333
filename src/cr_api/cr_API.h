c54b8b5493447736ae897ba403eb27ef8f890d33
InfiniteDreamX <jiperez11@uc.cl>
2020-05-16 12:02:26 -0400

    :sparkles: created struct crFILE

--- src/cr_api/cr_API.h
+++ src/cr_api/cr_API.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <stdlib.h>
 
+typedef struct cr_file crFILE;
+struct cr_file
+{
+   unsigned int *start; // Puntero al inicio del archivo
+   unsigned int *current;  // Puntero al lugar actual de lectura
+   char mode; // r o w
+   unsigned long size; // size del archivo, son 8 bytes
+   unsigned int references; // Cantidad total de referencias
+};
 char* disk_name;
 void cr_mount(char* diskname);
